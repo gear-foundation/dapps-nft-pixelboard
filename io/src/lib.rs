7ebbb9d5c33aff652f020d86b6398fe2ece574a6
Fluid <fluidcells@protonmail.ch>
2022-07-08 07:18:57 +0300

    Add the rest of files

---
+++ io/src/lib.rs
@@ -0,0 +1,137 @@
+#![no_std]
+
+use gear_lib::non_fungible_token::token::{TokenId, TokenMetadata};
+use gstd::{prelude::*, ActorId};
+
+pub type MinBlockSideLength = u16;
+pub type Color = u8;
+
+#[derive(Decode, Encode, TypeInfo, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Debug)]
+pub struct Rectangle {
+    pub upper_left_corner: Coordinates,
+    pub lower_right_corner: Coordinates,
+}
+
+impl
+    From<(
+        (MinBlockSideLength, MinBlockSideLength),
+        (MinBlockSideLength, MinBlockSideLength),
+    )> for Rectangle
+{
+    fn from(
+        rectangle: (
+            (MinBlockSideLength, MinBlockSideLength),
+            (MinBlockSideLength, MinBlockSideLength),
+        ),
+    ) -> Self {
+        Self {
+            upper_left_corner: rectangle.0.into(),
+            lower_right_corner: rectangle.1.into(),
+        }
+    }
+}
+
+#[derive(Decode, Encode, TypeInfo, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Debug)]
+pub struct Coordinates {
+    pub x: MinBlockSideLength,
+    pub y: MinBlockSideLength,
+}
+
+impl From<(MinBlockSideLength, MinBlockSideLength)> for Coordinates {
+    fn from(coordinates: (MinBlockSideLength, MinBlockSideLength)) -> Self {
+        Self {
+            x: coordinates.0,
+            y: coordinates.1,
+        }
+    }
+}
+
+#[derive(Decode, Encode, Default, Clone, Copy, TypeInfo)]
+pub struct Resolution {
+    pub width: MinBlockSideLength,
+    pub height: MinBlockSideLength,
+}
+
+impl From<(MinBlockSideLength, MinBlockSideLength)> for Resolution {
+    fn from(resolution: (MinBlockSideLength, MinBlockSideLength)) -> Self {
+        Self {
+            width: resolution.0,
+            height: resolution.1,
+        }
+    }
+}
+
+#[derive(Decode, Encode, TypeInfo, Clone, Copy, Debug)]
+pub struct Token(pub Rectangle, pub TokenInfo);
+
+#[derive(Decode, Encode, TypeInfo, Clone, Copy, Debug)]
+pub struct TokenInfo {
+    pub token_id: TokenId,
+    pub owner: ActorId,
+    pub pixel_price: Option<u128>,
+}
+
+#[derive(Decode, Encode, TypeInfo)]
+pub struct InitNFTPixelboard {
+    pub owner: ActorId,
+    pub min_block_side_length: MinBlockSideLength,
+    pub pixel_price: u128,
+    pub resolution: Resolution,
+    pub resale_commission_percentage: u8,
+    pub painting: Vec<Color>,
+
+    pub ft_program: ActorId,
+    pub nft_program: ActorId,
+}
+
+#[derive(Decode, Encode, TypeInfo)]
+pub enum NFTPixelboardAction {
+    Mint {
+        rectangle: Rectangle,
+        token_metadata: TokenMetadata,
+        painting: Vec<Color>,
+    },
+    Buy(TokenId),
+    PutUpForSale {
+        token_id: TokenId,
+        pixel_price: u128,
+    },
+    Paint {
+        token_id: TokenId,
+        painting: Vec<Color>,
+    },
+}
+
+#[derive(Decode, Encode, TypeInfo)]
+pub enum NFTPixelboardEvent {
+    Minted(TokenId),
+    Bought(TokenId),
+    ForSale(TokenId),
+    Painted(TokenId),
+}
+
+#[derive(Decode, Encode, TypeInfo)]
+pub enum NFTPixelboardState {
+    Painting,
+    Resolution,
+    PixelPrice,
+    MinBlockSideLength,
+    PixelInfo(Coordinates),
+    TokenInfo(TokenId),
+    ResaleCommissionPercentage,
+    FTProgram,
+    NFTProgram,
+}
+
+#[derive(Decode, Encode, TypeInfo)]
+pub enum NFTPixelboardStateReply {
+    Painting(Vec<Color>),
+    Resolution(Resolution),
+    PixelPrice(u128),
+    MinBlockSideLength(MinBlockSideLength),
+    PixelInfo(Token),
+    TokenInfo(Token),
+    ResaleCommissionPercentage(u8),
+    FTProgram(ActorId),
+    NFTProgram(ActorId),
+}
---
+++ tests/test.rs
@@ -0,0 +1,112 @@
+use ft_io::{FTAction, InitConfig as InitFT};
+use gstd::prelude::*;
+use gtest::{Program, System};
+use nft_io::InitNFT;
+use nft_pixelboard_io::*;
+
+pub fn init_ft(sys: &System) -> Program {
+    let ft_program = Program::from_file(sys, "./target/fungible_token.wasm");
+
+    assert!(ft_program
+        .send(
+            123123,
+            InitFT {
+                name: String::from("MyToken"),
+                symbol: String::from("MTK"),
+            },
+        )
+        .log()
+        .is_empty());
+
+    ft_program
+}
+
+pub fn init_nft(system: &System) -> Program {
+    let nft_program = Program::from_file(system, "./target/nft.wasm");
+
+    assert!(nft_program
+        .send(
+            123123,
+            InitNFT {
+                name: "Item".into(),
+                symbol: "ITM".into(),
+                base_uri: Default::default(),
+                royalties: Default::default(),
+            },
+        )
+        .log()
+        .is_empty());
+
+    nft_program
+}
+
+#[test]
+fn test() {
+    let system = System::new();
+    system.init_logger();
+    let prog = Program::current(&system);
+
+    let ft = init_ft(&system);
+
+    ft.send(123, FTAction::Mint(10000));
+
+    init_nft(&system);
+
+    let mut f = prog.send(
+        123123,
+        InitNFTPixelboard {
+            ft_program: 2.into(),
+            min_block_side_length: 10,
+            nft_program: 3.into(),
+            owner: 123124.into(),
+            painting: vec![0; 2500],
+            pixel_price: 1,
+            resale_commission_percentage: 0,
+            resolution: (50, 50).into(),
+        },
+    );
+    f.log().is_empty();
+
+    let d= prog
+        .send(
+            123,
+            NFTPixelboardAction::Mint {
+                token_metadata: Default::default(),
+                rectangle: ((1, 1), (20, 20)).into(),
+                painting: vec![1; 361]
+            }
+        );
+
+        dbg!(d.main_gas_burned());
+        dbg!(d.others_gas_burned());
+
+    assert!(d
+        .contains(&(123, NFTPixelboardEvent::Minted(0.into()).encode())));
+
+    assert!(prog
+        .send(
+            123,
+            NFTPixelboardAction::Mint {
+                token_metadata: Default::default(),
+                rectangle: ((22, 15), (24, 20)).into(),
+                painting: vec![7; 10]
+            }
+        )
+        .contains(&(123, NFTPixelboardEvent::Minted(1.into()).encode())));
+
+    let asdas: NFTPixelboardStateReply = prog.meta_state(NFTPixelboardState::Painting).unwrap();
+    if let NFTPixelboardStateReply::Painting(pai) = asdas {
+        for row in pai.chunks_exact(50) {
+            println!("{row:?}");
+        }
+    } else {
+        panic!("AAAAAA")
+    }
+
+    let asdas: NFTPixelboardStateReply = prog.meta_state(NFTPixelboardState::PixelInfo((23, 18).into())).unwrap();
+    if let NFTPixelboardStateReply::PixelInfo(pai) = asdas {
+        println!("{:?}", pai);
+    } else {
+        panic!("AAAAAA")
+    }
+}
